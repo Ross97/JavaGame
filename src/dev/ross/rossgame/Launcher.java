7219e99a8b9f5788ebee3ab071671ebeb8d6e718
Ross <rossphelan1997@gmail.com>
2018-04-29 11:46:39 +0100

    Added music file.

--- src/dev/ross/rossgame/Launcher.java
+++ src/dev/ross/rossgame/Launcher.java
@@ -1,9 +1,23 @@
 package dev.ross.rossgame;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Launcher {
-	public static void main(String[] args){
+	public static void main(String[] args) throws MalformedURLException{
+		
+		//In game music
+		URL url = new URL("https://raw.githubusercontent.com/Ross97/processingAssignment1/master/Main/data/urbanJungle.mp3"); //"http://www.edu4java.com/sound/back.wav"
+		AudioClip clip = Applet.newAudioClip(url);
+		clip.play();
+		
+		
 		Game game = new Game("Finder!", 750, 750);
 		game.start();
 	}
 }
+
+
+
